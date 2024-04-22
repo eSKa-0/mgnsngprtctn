345c21ec64b040b61b32eb7cacd294758f0c05e8
eSKa-0 <flavinito@proton.me>
2024-04-22 21:00:40 +0200

    udp packet sender

--- udpacket.py
+++ udpacket.py
@@ -1,15 +1,32 @@
 import socket
 
+
 class Udp:
     def __init__(self, targetIp, port, message):
         self.target = targetIp
         self.port = port
-        self.message = message
+        self.message = message.encode()
     def sendPacketUdp(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(self.message, (self.target, self.port))
-        print(f"Udp packet with message: {self.message} send to {self.target}:{self.port}")
 
-transmit1 = Udp("10.10.10.90", 5005, b"hello world")
+print(
+    """
+     ███▄ ▄███▓  ▄████  ███▄    █   ██████  ███▄    █   ▄████  ██▓███   ██▀███  ▄▄▄█████▓ ▄████▄  ▄▄▄█████▓ ███▄    █ 
+    ▓██▒▀█▀ ██▒ ██▒ ▀█▒ ██ ▀█   █ ▒██    ▒  ██ ▀█   █  ██▒ ▀█▒▓██░  ██▒▓██ ▒ ██▒▓  ██▒ ▓▒▒██▀ ▀█  ▓  ██▒ ▓▒ ██ ▀█   █ 
+    ▓██    ▓██░▒██░▄▄▄░▓██  ▀█ ██▒░ ▓██▄   ▓██  ▀█ ██▒▒██░▄▄▄░▓██░ ██▓▒▓██ ░▄█ ▒▒ ▓██░ ▒░▒▓█    ▄ ▒ ▓██░ ▒░▓██  ▀█ ██▒
+    ▒██    ▒██ ░▓█  ██▓▓██▒  ▐▌██▒  ▒   ██▒▓██▒  ▐▌██▒░▓█  ██▓▒██▄█▓▒ ▒▒██▀▀█▄  ░ ▓██▓ ░ ▒▓▓▄ ▄██▒░ ▓██▓ ░ ▓██▒  ▐▌██▒
+    ▒██▒   ░██▒░▒▓███▀▒▒██░   ▓██░▒██████▒▒▒██░   ▓██░░▒▓███▀▒▒██▒ ░  ░░██▓ ▒██▒  ▒██▒ ░ ▒ ▓███▀ ░  ▒██▒ ░ ▒██░   ▓██░
+    ░ ▒░   ░  ░ ░▒   ▒ ░ ▒░   ▒ ▒ ▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒  ░▒   ▒ ▒▓▒░ ░  ░░ ▒▓ ░▒▓░  ▒ ░░   ░ ░▒ ▒  ░  ▒ ░░   ░ ▒░   ▒ ▒ 
+    ░  ░      ░  ░   ░ ░ ░░   ░ ▒░░ ░▒  ░ ░░ ░░   ░ ▒░  ░   ░ ░▒ ░       ░▒ ░ ▒░    ░      ░  ▒       ░    ░ ░░   ░ ▒░
+    ░      ░   ░ ░   ░    ░   ░ ░ ░  ░  ░     ░   ░ ░ ░ ░   ░ ░░         ░░   ░   ░      ░          ░         ░   ░ ░ 
+           ░         ░          ░       ░           ░       ░             ░              ░ ░                        ░ 
+                                                                                     ░                            
+""")
+ip = str(input("to what ip would you like to send a packet to?\n"))
+pt = int(input("what port should it be send to?\n"))
+ms = input("what should the message be?\n")
+transmit1 = Udp(ip, pt, ms)
 transmit1.sendPacketUdp()
+print(f"Udp packet with message: {ms} send to {ip}:{pt}")
 print("quitting program")
