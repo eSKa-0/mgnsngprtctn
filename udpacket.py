6f4c84117656899592aa0f673ad2b0308051f551
eSKa-0 <flavinito@proton.me>
2024-04-23 17:14:18 +0200

    simple udp packet sender

--- udpacket.py
+++ udpacket.py
@@ -9,7 +9,7 @@ class Udp:
     def sendPacketUdp(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(self.message, (self.target, self.port))
-
+bool = True
 print(
     """
      █    ██ ▓█████▄  ██▓███   ▄▄▄       ▄████▄   ██ ▄█▀▓█████▄▄▄█████▓
@@ -23,10 +23,14 @@ print(
        ░        ░                   ░  ░░ ░      ░  ░      ░  ░        
               ░                         ░                              
     """)
-ip = str(input("to what ip would you like to send a packet to?\n"))
-pt = int(input("what port should it be send to?\n"))
-ms = input("what should the message be?\n")
-transmit1 = Udp(ip, pt, ms)
-transmit1.sendPacketUdp()
-print(f"Udp packet with message: {ms} send to {ip}:{pt}")
+while bool == True:
+    ip = str(input("to what ip would you like to send a packet to?\n"))
+    if ip == "":
+        bool = False
+    else:
+        pt = int(input("what port should it be send to?\n"))
+        ms = input("what should the message be?\n")
+        transmit1 = Udp(ip, pt, ms)
+        transmit1.sendPacketUdp()
+        print(f"Udp packet with message: {ms} send to {ip}:{pt}")
 print("Quitting program...")
