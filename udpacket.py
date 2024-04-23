01441544dc8965504c3b877b90b17e95f31c03ae
eSKa-0 <166024351+eSKa-0@users.noreply.github.com>
2024-04-23 09:06:12 +0200

    Update udpacket.py

--- udpacket.py
+++ udpacket.py
@@ -14,11 +14,11 @@ class Tcp:
     def __init__(self, targetIp, port, message):
         self.target = targetIp
         self.port = port
-        self.message = message.encode(utf-8)
+        self.message = message.encode()
     def sendPacketTcp(self):
         sock = socket.socket(socket.AF_INET, socket.TCP_STREAM)
         sock.sendto(self.message, (self.target, self.port))
-
+bool = True
 print(
     """
      █    ██ ▓█████▄  ██▓███   ▄▄▄       ▄████▄   ██ ▄█▀▓█████▄▄▄█████▓
@@ -32,19 +32,22 @@ print(
        ░        ░                   ░  ░░ ░      ░  ░      ░  ░        
               ░                         ░                              
     """)
-md = int(input("Would you like to send a TCP or an UDP packet? (1/2)\n")
-if md == 1:
-    ip = str(input("to what ip would you like to send a packet to?\n"))
-    pt = int(input("what port should it be send to?\n"))
-    ms = input("what should the message be?\n")
-    transmit = Udp(ip, pt, ms)
-    transmit.sendPacketUdp()
-    print(f"UDP packet with message: {ms} send to {ip}:{pt}")
-if md == 2:
-    ip = str(input("to what ip would you like to send a packet to?\n"))
-    pt = int(input("what port should it be send to?\n"))
-    ms = input("what should the message be?\n")
-    transmit = Tcp(ip, pt, ms)
-    transmit.sendPacketTcp
-    print(f"TCP packet with message: {ms} send to {ip}:{pt}")
+while bool:
+    md = input("Would you like to send a TCP or an UDP packet? (1/2)\n")
+    if md == "1":
+        ip = str(input("to what ip would you like to send a packet to?\n"))
+        pt = int(input("what port should it be send to?\n"))
+        ms = input("what should the message be?\n")
+        transmit = Udp(ip, pt, ms)
+        transmit.sendPacketUdp()
+        print(f"UDP packet with message: {ms} send to {ip}:{pt}")
+    if md == "2":
+        ip = str(input("to what ip would you like to send a packet to?\n"))
+        pt = int(input("what port should it be send to?\n"))
+        ms = input("what should the message be?\n")
+        transmit = Tcp(ip, pt, ms)
+        transmit.sendPacketTcp
+        print(f"TCP packet with message: {ms} send to {ip}:{pt}")
+    if md == "":
+        bool = False
 print("Quitting program...")
